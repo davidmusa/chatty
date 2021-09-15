ce35bfce5c82120384122fbe6abcfebc4b6784a8
David M <davidmusa1@hotmail.com>
2021-09-15 14:12:42 -0400

    Added some design to the page

--- src/Chatty/index.php
+++ src/Chatty/index.php
@@ -48,9 +48,111 @@ mysqli_close($link);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Chatty</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+
+<script>
+
+function show_func(){
+ 
+ var element = document.getElementById("");
+    element.scrollTop = element.scrollHeight;
+  
+ }
+
+ </script>
+ <!--  -->
+<form id="myform" action="index.php" method="POST" >
+<div class="inner_div" id="chathist">
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $db_name = "chatty";
+    $con = new mysqli($host, $user, $pass, $db_name);
     
+    $query = "SELECT * FROM chats";
+    $run = $con->query($query);
+    $i=0;
+    
+    while($row = $run->fetch_array()) :
+    if($i==0){
+    $i=5;
+    $first=$row;
+ ?>
+
+
+    <?php echo $row['msg']; ?></span> <br/>
+    <?php echo $row['uname']; ?>,1
+    <?php echo $row['dt']; ?>
+
+
+<br/><br/>
+
+<?php
+    }
+    else
+    {
+    if($row['uname']!=$first['uname'])
+    {
+?>
+    <div id="message1" class="message1">
+    <div style="color: blue">Message: </div><?php echo $row['msg']; ?><br>
+    <div style="color: blue">Username: </div><?php echo $row['uname']; ?>,
+             <?php echo $row['dt']; ?>
+    </div>
+
+<br/><br/>
+
+<?php
+
+}
+else
+{
+
+?>
+
+    <?php echo $row['msg']; ?>
+    <?php echo $row['uname']; ?>,
+    <?php echo $row['dt']; ?>
+
+<br/><br/>
+
+<?php
+}
+}
+endwhile;
+?>
+
+    <footer>
+
+        <table>
+
+            <tr>
+            <th>
+                <input  class="input1" type="text"
+                        id="uname" name="uname"
+                        placeholder="Name">
+                </th>
+                <th>
+                    <textarea id="msg" name="msg"
+                        rows='3' cols='50'
+                        placeholder="Type your message">
+                    </textarea></th>
+                <th>
+                    <input class="input2" type="submit"
+                    id="submit" name="submit" value="send">
+                </th>               
+            </tr>
+
+        </table> 
+
+    </footer>
+</form>
+</main>   
+</div>
+ 
 </body>
 </html>
