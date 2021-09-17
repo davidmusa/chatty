f0e9ead0d08e0f9b57c2b3f745a20eca7ac8fdad
David M <davidmusa1@hotmail.com>
2021-09-17 08:45:09 -0400

    Restarted all the code

--- src/Chatty/message.php
+++ src/Chatty/message.php
@@ -1,14 +1,14 @@
 <?php
-    $query = "SELECT * FROM chats LIMIT 1000";
-    $exec = $link->query($query);
 
-    $output = "";
-    while ($row = $exec->fetch_array()) {
-        $message = $row['msg'];
-        $username = $row['uname'];
-        $time = $row['dt'];
-        $output .= "<br><div class='message'>[" . $time . "] " . $username . " > " . $message . "</div>";
-    }
+    /*
+    *   Date:
+    *   Author:
+    *   Description:
+    */
 
-?>
+    // Initialize the session
+    session_start();
 
+    require_once "config.php";
+
+    
