cc06b19f94b9df9cf71fea9aed541ed0fe6edda1
David M <davidmusa1@hotmail.com>
2021-09-17 14:06:00 -0400

    Created Ajax

--- src/Chatty/index.php
+++ src/Chatty/index.php
@@ -6,3 +6,49 @@
     * Description: This file contains code.
     */
 
+    // Include config file
+    require_once "config.php";
+    require_once "message.php";
+    
+?>
+
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Chatty</title>
+            <link rel="stylesheet" href="styles.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        </head>
+    <body>
+
+        <div id="chathist">
+            <?php
+                $query = "SELECT * FROM chats ORDER BY dt DESC LIMIT 100";
+                $exec = $link->query($query);
+                while ($row = $exec->fetch_array()){
+                    echo $output;
+                }
+            ?>
+        </div>
+
+        <form>
+            <label for="username">Username</label>
+            <input type="text" name="username"><br><br>
+            <label for="message">Message</label>
+            <input type="text" name="message"><br><br>
+            <button style="height: 30px; width: 80px;" id="btn">Send</button>
+        </form>
+
+        <script>
+            $(document).ready(function(){
+                $("btn").click(function(){
+                    $("#chathist").load("message.php");
+                });
+            });    
+        </script>
+
+    </body>
+</html>
--- src/Chatty/message.php
+++ src/Chatty/message.php
@@ -11,4 +11,42 @@
 
     require_once "config.php";
 
+    //input validation
+    $errors = Array();
+
+    $query = "SELECT * FROM chats ORDER BY dt DESC LIMIT 100";
+    $exec = $link->query($query);
+
+    $output = "";
+    while ($row = $exec->fetch_array()) {
+        $message = $row['msg'];
+        $username = $row['uname'];
+        $time = $row['dt'];
+        $output .= "<br><div class='message'>[" . $time . "] " . $username . " > " . $message . "</div>";
+    }
     
+    if(isset($_POST)) {
+        if(isset($_POST['uname'])) {
+            $username = htmlspecialchars($_POST['uname']);
+            if (empty($username)) {
+            } else {
+             $errors[] = "Please write your name";
+            } 
+        } else {
+            $errors [] = "You did not submit anything";
+        }
+ 
+        if (isset($_POST['msg'])){
+            $message = htmlspecialchars($_POST['msg']);
+            if (empty($message)) {
+             } else {
+                 $errors[] = "Please write your message";
+             }
+        } else {
+            $errors [] = "You did not submit anything";
+        }
+     }
+   
+?>
+
+       
