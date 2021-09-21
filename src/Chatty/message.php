bc9e37c0f7df4c34f6ced54983f462931db1e5e1
David M <davidmusa1@hotmail.com>
2021-09-21 10:31:38 -0400

    Fatal error fixed working on INSERT INTO DB

--- src/Chatty/message.php
+++ src/Chatty/message.php
@@ -11,12 +11,30 @@
     //3. BUSINESS LOGIC
     //4. OUTPUT
 
-    
-
     require_once "config.php";
 
     //input validation
     $errors = Array();
+
+    if(isset($_POST['submit'])){
+
+        $username= mysqli_real_escape_string(
+            $link, $_REQUEST['uname']);
+        $message = mysqli_real_escape_string(
+            $link, $_REQUEST['msg']);
+        date_default_timezone_set('America/Toronto');
+            $time_date=date('y-m-d h:ia');
+        
+    $sql = "INSERT INTO chats (uname, msg, dt) VALUES ('$username', '$message', '$time_date')";
+    if(mysqli_query($link, $sql)){
+        ;
+    } else{
+        echo "ERROR: Message not sent!!!";
+    }
+     // Close connection
+    mysqli_close($link);
+
+    }
 
     $query = "SELECT * FROM chats ORDER BY dt DESC LIMIT 100";
     $exec = $link->query($query);
@@ -28,38 +46,28 @@
         $time = $row['dt'];
         $output .= "<br><div class='message'>[" . $time . "] " . $username . " > " . $message . "</div>";
     }
-    
+
     if(isset($_POST)) {
         if(isset($_POST['uname'])) {
             $username = htmlspecialchars($_POST['uname']);
             if (empty($username)) {
             } else {
-             $errors[] = "Please write your name";
+                $errors[] = "Please write your name";
             } 
             } else {
             $errors [] = "You did not submit anything";
             }
- 
+
         if (isset($_POST['msg'])) {
             $message = htmlspecialchars($_POST['msg']);
             if (empty($message)) {
             } else {
-                 $errors[] = "Please write your message";
+                    $errors[] = "Please write your message";
             }
             } else {
             $errors [] = "You did not submit anything";
             }
     }
-   
-    $sql = $link->prepare("INSERT INTO chats (uname, msg, dt) VALUES (?, ?, ?)");
-    $sql->bind_param($username, $message, $time);
-        if (mysqli_query($link, $sql)){
-            ;
-        } else{
-            echo "ERROR: Message not sent!!!";
-        }
-        // Close connection
-        mysqli_close($link); 
 
 ?>
 
